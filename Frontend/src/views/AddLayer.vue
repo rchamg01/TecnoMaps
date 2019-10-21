2cd3c2c091a9dd1ad2ecd110d405365085c28820
rchamg01 <rchamg01@estudiantes.unileon.es>
2019-10-21 12:52:12 +0200

    mejora interfaz

--- Frontend/src/views/AddLayer.vue
+++ Frontend/src/views/AddLayer.vue
@@ -1,7 +1,9 @@
 <template>
 <v-app>
-    <v-card flat fill-height>
-    
+    <v-card flat>
+    <v-toolbar flat tile color="rgb(255,255,255)">
+                    <v-toolbar-title>LAYER</v-toolbar-title>
+                  </v-toolbar>
      <v-snackbar
         v-model="snackbar"
         absolute
@@ -9,49 +11,51 @@
         right
         color="success"
       >
-      
         <span>Registration successful!</span>
         <v-icon dark>mdi-checkbox-marked-circle</v-icon>
       </v-snackbar>
+
       <v-form ref="form" @submit.prevent="submit">
       <v-container grid-list-xl>
       <v-layout row justify-space-between>
 
-        <v-flex cols="6" md="4">
+        <v-flex xs4>
           <v-text-field
             v-model="form.layerName"
             :rules="rules.name"
             label="Name"
-            placeholder="layer name"
+            placeholder="Layer name"
             hint="required field"
-            outlined
+            
             clearable
             required
           ></v-text-field>
         </v-flex>
 
-        <v-flex cols="6" md="4">
+        <v-flex xs4>
           <v-text-field
             label="Description"
-            placeholder="layer description"
-            hint="For example, xxxxx"
-            outlined
+            placeholder="Layer description"
+            hint="Optional field"
+            
           ></v-text-field>
         </v-flex>
 
-        <v-flex cols="6" md="4">
+        <v-flex xs4>
           <v-text-field
           v-model="form.layerSource"
             :rules="rules.source"
             label="Source"
-            placeholder="layer source"
+            placeholder="Layer source"
             hint="required field"
-            outlined
+            
           ></v-text-field>
         </v-flex>
         </v-layout>
-        <v-layout row wrap>
-        <v-flex cols="6" md="4">
+        <v-layout row justify-space-between>
+        
+        <v-flex xs4>
+        
           <v-slider
                 v-model="form.opacity"
                 :rules="rules.opacity"
@@ -60,32 +64,56 @@
                 min="1"
                 max="100"
                 thumb-label
+                thumb-size="25"
+                height="62"
+                dense
               ></v-slider>
         </v-flex>
 
-        <v-flex cols="6" md="4">
+        <v-flex xs4>
           <v-text-field
-            label="Outlined"
-            placeholder="Placeholder"
-            hint="For example, flowers or used cars"
-            outlined
+            label="Layer visibility maximum scale"
+            prepend-inner-icon="zoom_in"           
+            placeholder="1:"
           ></v-text-field>
         </v-flex>
 
-        <v-flex cols="6" md="4">
+        <v-flex xs4>
           <v-text-field
-            label="Outlined"
-            placeholder="Placeholder"
-            hint="For example, flowers or used cars"
+            label="Layer visibility minimum scale"
+            prepend-inner-icon="zoom_out"  
+            placeholder="1:"
+            
             outlined
           ></v-text-field>
         </v-flex>
-
-        
       </v-layout>
+      <v-layout row justify-space-between>
+        <v-flex xs4>
+      <v-checkbox
+              label="Restricted layer"
+              color="green"
+            >
+        </v-checkbox>
+        </v-flex>
+        <v-flex xs4>
+      <v-checkbox
+              label="Proxy"
+              color="green"
+            >
+        </v-checkbox>
+        </v-flex>
+        <v-flex xs4>
+      <v-checkbox
+              label="Secured"
+              color="green"
+            >
+        </v-checkbox>
+        </v-flex>
+        </v-layout>
        </v-container>
       <v-card-actions>
-          <v-btn text depressed outlined @click="resetForm">Cancel</v-btn>
+          <v-btn text depressed outline @click="resetForm">Cancel</v-btn>
           <v-spacer></v-spacer>
           <v-btn
             :disabled="!formIsValid"
@@ -93,7 +121,7 @@
             color="primary"
             type="submit"
             depressed
-            outlined
+            outline
           >Register</v-btn>
         </v-card-actions>
   </v-form>
