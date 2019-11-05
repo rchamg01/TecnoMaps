9ca95af5d04d5fa6554df1da5cbee7a3cf7e9667
rchamg01 <rchamg01@estudiantes.unileon.es>
2019-11-05 11:30:08 +0100

    Login mejorado + cambios en la interfaz

--- Frontend/src/views/Login.vue
+++ Frontend/src/views/Login.vue
@@ -6,106 +6,107 @@
           <v-flex align="center" name="error1">
             <v-flex class="grow">{{alertContent}}</v-flex>
             <v-flex class="shrink">
-              <v-btn color="info" outlined @click="alert = !alert" left>Okey</v-btn>
+              <v-btn color="info" outlined @click="alert = !alert" left>OK</v-btn>
             </v-flex>
           </v-flex>
         </v-alert>
-        <v-layout align-center column justify-center>
+        <v-layout align-center row justify-center>
           <h1 class="display-4 font-weight-bold">
             <span style="color: white">TecnoMaps</span>
           </h1>
         </v-layout>
         <v-content>
           <v-container grid-list-md text-xs-center>
-            <v-layout row wrap fill-height>
-              <v-flex d-flex>
-                <v-card flat class="elevation">
-                  <v-toolbar flat class="white--text" color="#907A55">
-                    <v-toolbar-title>Login</v-toolbar-title>
-                  </v-toolbar>
-                  <v-card-text>
-                    <v-form>
-                      <v-text-field
-                        prepend-icon="person"
-                        name="login"
-                        label="Login"
-                        type="text"
-                        v-model="loginName"
-                      ></v-text-field>
-                      <v-text-field
-                        prepend-icon="lock"
-                        name="password"
-                        label="Password"
-                        type="password"
-                        v-model="loginPass"
-                      ></v-text-field>
-                    </v-form>
-                  </v-card-text>
-                  <v-toolbar flat class="white--text" color="white"></v-toolbar>
-                  <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn
-                      round
-                      depressed
-                      class="white--text"
-                      color="#907A55"
-                      v-on:click="login"
-                    >Login</v-btn>
-                  </v-card-actions>
-                </v-card>
-                <v-card flat class="elevation">
-                  <v-toolbar flat class="white--text" color="#907A55">
-                    <v-toolbar-title>Sing up</v-toolbar-title>
-                    <v-spacer></v-spacer>
-                  </v-toolbar>
-                  <v-card-text>
-                    <v-form>
-                      <v-text-field
-                        prepend-icon="alternate_email"
-                        name="e-mail"
-                        label="E-mail"
-                        type="text"
-                        v-model="email"
-                      ></v-text-field>
-                      <v-text-field
-                        prepend-icon="person"
-                        name="signName"
-                        label="Username"
-                        type="text"
-                        v-model="signName"
-                      ></v-text-field>
-                      <v-text-field
-                        prepend-icon="lock"
-                        name="password"
-                        label="Password"
-                        type="password"
-                        v-model="signPass"
-                      ></v-text-field>
-                    </v-form>
-                  </v-card-text>
-                  <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn
-                      round
-                      depressed
-                      class="white--text"
-                      color="#907A55"
-                      @click="register"
-                    >Sign up</v-btn>
-                  </v-card-actions>
-                </v-card>
+            <v-layout row justify-space-between fill-height>
+              <v-flex xs6>
+                <v-carousel cycle height="330" hide-delimiters>
+                  <v-carousel-item v-for="(slide, i) in slides" :key="i"></v-carousel-item>
+                </v-carousel>
+              </v-flex>
+              <v-flex xs6>
+                <v-tabs v-model="model" centered color="#907A55" dark slider-color="yellow">
+                  <v-tab ripple>SIGN UP</v-tab>
+                  <v-tab ripple>LOGIN IN</v-tab>
+
+                  <v-tab-item>
+                    <!--item-->
+                    <v-card flat class="elevation">
+                      <v-card-text>
+                        <v-form>
+                          <v-text-field
+                            prepend-icon="alternate_email"
+                            name="e-mail"
+                            label="E-mail"
+                            type="text"
+                            v-model="email"
+                          ></v-text-field>
+                          <v-text-field
+                            prepend-icon="person"
+                            name="signName"
+                            label="Username"
+                            type="text"
+                            v-model="signName"
+                          ></v-text-field>
+                          <v-text-field
+                            prepend-icon="lock"
+                            name="password"
+                            label="Password"
+                            type="password"
+                            v-model="signPass"
+                          ></v-text-field>
+                        </v-form>
+                      </v-card-text>
+                      <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn
+                          round
+                          depressed
+                          class="white--text"
+                          color="#907A55"
+                          @click="register"
+                        >Sign up</v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-tab-item>
+                  <v-tab-item>
+                    <!--item-->
+                    <v-card flat class="elevation">
+                      <v-card-text>
+                        <v-form>
+                          <v-text-field
+                            prepend-icon="person"
+                            name="login"
+                            label="Login"
+                            type="text"
+                            v-model="loginName"
+                          ></v-text-field>
+                          <v-text-field
+                            prepend-icon="lock"
+                            name="password"
+                            label="Password"
+                            type="password"
+                            v-model="loginPass"
+                          ></v-text-field>
+                        </v-form>
+                      </v-card-text>
+                      <v-toolbar flat class="white--text" color="white"></v-toolbar>
+                      <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn
+                          round
+                          depressed
+                          class="white--text"
+                          color="#907A55"
+                          v-on:click="login"
+                        >Login</v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-tab-item>
+                </v-tabs>
               </v-flex>
             </v-layout>
           </v-container>
-          <v-carousel cycle height="300" hide-delimiter-background show-arrows-on-hover>
-            <v-carousel-item v-for="(slide, i) in slides" :key="i">
-              <v-sheet :color="colors[i]" height="100%">
-                <v-flex class="text-xs-center" align="center" justify="center" name="slide">
-                  <div class="display-3">{{ slide }} Slide</div>
-                </v-flex>
-              </v-sheet>
-            </v-carousel-item>
-          </v-carousel>
+          <!-- -->
         </v-content>
       </v-parallax>
     </v-content>
@@ -118,19 +119,13 @@ export default {
   components: {},
   data() {
     return {
+      model: "tab-2",
       address: "http://" + location.host + ":3000",
       loginName: null,
       loginPass: null,
       email: null,
       signName: null,
       signPass: null,
-      colors: [
-        "indigo",
-        "warning",
-        "pink darken-2",
-        "red lighten-1",
-        "deep-purple accent-4"
-      ],
       slides: ["First", "Second", "Third", "Fourth", "Fifth"],
       alert: false,
       alertContent: ""
@@ -202,10 +197,6 @@ export default {
 header {
   background-color: white;
   text-align: center;
-}
-
-.elevation {
-  border-radius: 30px;
 }
 
 .footer {
