bdf95b293a8aa01dbd2b05e8d187a000196601d7
rchamg01 <rchamg01@estudiantes.unileon.es>
2019-11-25 10:27:11 +0100

    Update Users.vue

--- Frontend/src/views/Users.vue
+++ Frontend/src/views/Users.vue
@@ -29,125 +29,7 @@
       <span>An error ocurred</span>
       <v-icon dark>mdi-close</v-icon>
     </v-snackbar>
-    <!--<v-list>
-      <v-list-group v-for="layer in layers" v-bind:key="layer.id">
-        <template v-slot:activator>
-          <v-card flat>
-            <v-card-title>
-              <v-icon left>map</v-icon>
-              {{layer.name}}
-            </v-card-title>
-          </v-card>
-        </template>
-
-        <v-card flat>
-          <v-form ref="form" @submit.prevent="submit">
-            <v-container grid-list-xl>
-              <v-layout row justify-space-between>
-                <v-flex xs4>
-                  <v-text-field
-                    v-model="layer.layerNameModel"
-                    :rules="rules.name"
-                    label="Name"
-                    placeholder="Name"
-                    hint="Required field"
-                    clearable
-                    required
-                  ></v-text-field>
-                </v-flex>
-
-                <v-flex xs4>
-                  <v-text-field
-                    v-model="layer.layerName"
-                    :rules="rules.layerName"
-                    label="Layer Name"
-                    placeholder="Layer Name"
-                    hint="Required field"
-                    clearable
-                    required
-                    disabled
-                  ></v-text-field>
-                </v-flex>
-
-                <v-flex xs4>
-                  <v-text-field
-                    v-model="layer.url"
-                    :rules="rules.source"
-                    label="Source"
-                    placeholder="Layer URL"
-                    hint="Required field"
-                    disabled
-                  ></v-text-field>
-                </v-flex>
-              </v-layout>
-              <v-layout row justify-space-between>
-                <v-flex xs4>
-                  <v-slider
-                    v-model="layer.opacity"
-                    :rules="rules.opacity"
-                    color="orange"
-                    label="Opacity"
-                    min="1"
-                    max="100"
-                    thumb-label
-                    thumb-size="25"
-                    height="62"
-                    dense
-                  ></v-slider>
-                </v-flex>
-
-                <v-flex xs4>
-                  <v-text-field
-                    v-model="layer.description"
-                    label="Description"
-                    placeholder="Layer description"
-                    hint="Optional field"
-                  ></v-text-field>
-                </v-flex>
-
-                <v-flex xs4>
-                  <v-text-field
-                    label="Layer visibility minimum scale"
-                    prepend-inner-icon="zoom_out"
-                    placeholder="1:"
-                    outlined
-                  ></v-text-field>
-                </v-flex>
-              </v-layout>
-              <v-layout row justify-space-between>
-                <v-flex xs4>
-                  <v-checkbox label="Visible" color="green" v-model="form.visible"></v-checkbox>
-                </v-flex>
-                <v-flex xs4>
-                  <v-checkbox disabled label="Proxy" color="green"></v-checkbox>
-                </v-flex>
-                <v-flex xs4>
-                  <v-checkbox disabled label="Secured" color="green"></v-checkbox>
-                </v-flex>
-              </v-layout>
-            </v-container>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-
-              <v-btn color="red" outline depressed @click="showDialog(layer)">Delete</v-btn>
-
-              <v-dialog v-model="dialog" width="500">
-                <v-card>
-                  <v-card-title class="headline blue-grey darken-1">Delete</v-card-title>
-                  <v-card-text>Are you sure you want to delete this layer?</v-card-text>
-                  <v-card-actions>
-                    <v-btn color="grey" outline depressed @click="dialog = false">Cancel</v-btn>
-                    <v-spacer></v-spacer>
-                    <v-btn color="red" outline depressed @click="deleteLayer()">DELETE</v-btn>
-                  </v-card-actions>
-                </v-card>
-              </v-dialog>
-              <v-btn text color="primary" depressed outline @click="save(layer)">Save</v-btn>
-            </v-card-actions>
-          </v-form>
-        </v-card>
-      </v-list-group>
-    </v-list>-->
+   
     <v-data-table :headers="headers" :items="users" hide-actions>
       <template slot="headerCell" slot-scope="{ header }">
         <span
@@ -168,29 +50,12 @@
 <script>
 export default {
   data() {
-    const defaultForm = Object.freeze({
-      name: "",
-      layerName: "",
-      layerSource: "",
-      opacity: 50,
-      desc: "",
-      visible: true
-    });
-
     return {
-      form: Object.assign({}, defaultForm),
-      rules: {
-        name: [val => (val || "").length > 0 || "This field is required"],
-        source: [val => (val || "").length > 0 || "This field is required"],
-        layerName: [val => (val || "").length > 0 || "This field is required"]
-      },
       dialog: false,
       conditions: false,
       snackbarError: false,
       snackbarSuccess: false,
       snackbarSuccessDeleted: false,
-      defaultForm,
-      dialogId: "",
 
       headers: [
         {
@@ -214,41 +79,15 @@ export default {
           value: "active"
         }
       ],
-      users: [
-        /* {
-          userName: "rchamg01",
-          email: "rchamg01@estudiantes.unileon.es",
-          password: "1234",
-          id: "1998"
-        },
-        {
-          userName: "aferns16",
-          email: "aferns16@estudiantes.unileon.es",
-          password: "1236",
-          id: "1999"
-        },
-        {
-          userName: "gneid92",
-          email: "gneid92@estudiantes.unileon.es",
-          password: "5236",
-          id: "2000"
-        }*/
-      ]
+      users: []
     };
   },
-  computed: {
-    formIsValid() {
-      return this.form.name && this.form.layerSource && this.form.layerName;
-    }
+  computed: {    
   },
   mounted() {
     this.requestUsers();
   },
   methods: {
-    showDialog(layer) {
-      this.dialog = true;
-      this.dialogId = layer.id;
-    },
     requestUsers() {
       var data = this.$store.getters.getUser;
       this.$store
@@ -261,56 +100,6 @@ export default {
           this.snackbarError = true;
         });
     }
-    /*requestLayers() {
-      var data = this.$store.getters.getUser;
-      this.$store
-        .dispatch("getLayers", data)
-        .then(() => {
-          this.layers = this.$store.getters.getLayers;
-          for (var i = 0; i < this.layers.length; i++) {
-            this.layers[i].layerNameModel = this.layers[i].name;
-          }
-        })
-        .catch(err => {
-          this.snackbarError = true;
-        });
-    },
-    save(layer) {
-      if (this.form.opacity == null) this.form.opacity = 50;
-      console.log(layer.id);
-      var data = {
-        name: layer.layerNameModel,
-        opacity: layer.opacity,
-        desc: layer.description,
-        visible: layer.visible,
-        id: layer.id,
-        idUser: this.$store.getters.getUser.id
-      };
-      this.$store
-        .dispatch("updateLayer", data)
-        .then(() => {
-          this.snackbarSuccess = true;
-          this.requestLayers();
-        })
-        .catch(err => {
-          this.snackbarError = true;
-        });
-    },
-    deleteLayer() {
-      var data = {
-        id: this.dialogId,
-        idUser: this.$store.getters.getUser.id
-      };
-      this.$store
-        .dispatch("deleteLayer", data)
-        .then(() => {
-          this.snackbarSuccessDeleted = true;
-          this.requestLayers();
-        })
-        .catch(err => {
-          this.snackbarError = true;
-        });
-    }*/
   }
 };
 </script>
